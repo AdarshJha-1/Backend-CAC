71d841c33dce070b35a249a22f7374e32d9d1128
AdarshJha <adarsh2005j@gmail.com>
2024-03-23 11:23:05 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("MongoDB ERROR :: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is listing at PORT :: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB Connection Failed !!! ", error);
+  });
 
+  
 /*
 import mongoose from "mongoose"; 
 import express from "express";
